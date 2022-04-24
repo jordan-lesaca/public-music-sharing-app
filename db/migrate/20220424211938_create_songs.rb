67b6e6fc625eb38cc87b5eb93cd9689764b2f3cb
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-04-24 15:04:08 -0700

    added schema attributes and seed data

--- db/migrate/20220424211938_create_songs.rb
+++ db/migrate/20220424211938_create_songs.rb
@@ -1,6 +1,12 @@
 class CreateSongs < ActiveRecord::Migration[6.1]
   def change
     create_table :songs do |t|
+      t.string :title
+      t.string :artist
+      t.string :featured_artist
+      t.string :genre
+      t.integer :year
+      t.string :posted_by
 
       t.timestamps
     end
--- db/migrate/20220424211959_create_profiles.rb
+++ db/migrate/20220424211959_create_profiles.rb
@@ -1,6 +1,11 @@
 class CreateProfiles < ActiveRecord::Migration[6.1]
   def change
     create_table :profiles do |t|
+      t.string :name
+      t.string :favorite_genre
+      t.string :pronouns
+      t.belongs_to :user, null: false, foreign_key: true
+
 
       t.timestamps
     end
