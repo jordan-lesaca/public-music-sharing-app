baa70e6a3bb4ff450626e1ee7da1ec57a01c60f7
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-04-24 15:42:26 -0700

    migrated, created users, edited serializers to show user id and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :favorites, dependent: :destroy
+    has_many :songs, through: :favorites
+    has_one :profile, dependent: :destroy
 end
--- client/src/components/Songs.js
+++ client/src/components/Songs.js
@@ -1,4 +1,14 @@
+import {useState, useEffect} from 'react'
+
 function Songs(){
+    const [ songs, setSongs ] = useState([])
+
+    useEffect(() => {
+        fetch(`/songs`)
+        .then((r) => r.json())
+        .then(songs => setSongs(songs))    
+        }, [])
+
     return (
         <div>
             <h1> Song Page </h1>
