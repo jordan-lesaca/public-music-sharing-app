bd3a8887c55df6f6feddc73262218d1fbd489831
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-04-25 16:37:41 -0700

    made home link and profile link separate

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -11,7 +11,8 @@ function NavBar({ onLogout }){
 
   return(
     <div className="NavBar">
-      <NavLink style={{ marginRight: '10px'}} to='/'>Profile</NavLink>
+      <NavLink style={{ marginRight: '10px'}} to='/'>Home</NavLink>
+      <NavLink style={{ marginRight: '10px'}} to='/profile'>Profile</NavLink>
       <NavLink style={{ marginRight: '10px'}} to='/songs'>Songs</NavLink> 
       <NavLink style={{ marginRight: '10px'}} to='/mysongs'>MySongs</NavLink> 
       <NavLink style={{ marginRight: '10px'}} to='/favorites'>Favorites</NavLink> 
