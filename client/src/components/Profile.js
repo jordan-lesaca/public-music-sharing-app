6a5d73fa9756754215076a559560d0b14d761a2e
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-04-25 16:59:25 -0700

    created profile forms

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,8 +1,29 @@
 import { useState, useEffect } from 'react'
 import ProfileCard from '../cards/ProfileCard'
+import ProfileForm from '../forms/ProfileForm'
+import EditProfileForm from '../forms/EditProfileForm'
 
 function Profile({ user }){
   const [ profile, setProfile ] = useState([])
+  const [ editButton, setEditButton ] = useState("Show Edit Form")
+  const [ editForm, setEditForm ] = useState(false)
+
+  function addProfile(profile){
+    setProfile(profile)
+  }
+
+  function handleEditButton(){
+    setEditForm(!editForm)
+    !editForm ? 
+    setEditButton("Hide Edit Form") : setEditButton("Show Edit Form")
+  }
+
+  function editProfile(p){
+    if (profile.id === p.id){
+      return profile
+      }
+        setProfile(p)
+    }
 
   useEffect(() => {
     fetch(`profiles/${user.id}`,)
@@ -10,11 +31,26 @@ function Profile({ user }){
     .then(profile => setProfile(profile)) 
   }, [])  // eslint-disable-line react-hooks/exhaustive-deps
 
+
+  if (!user.profile) return <ProfileForm user={user} addProfile={addProfile} />
+
   return (
     <div>
       <h1 className="page-title">Profile Page</h1>
         <ProfileCard user={user} 
-        profile={profile} />
+        profile={profile} 
+        handleEditButton={handleEditButton} 
+        editButton={editButton}/> 
+      <div>
+        {editForm ?
+          <EditProfileForm 
+          handleEditButton={handleEditButton}
+          setEditForm={setEditForm}
+          editProfile={editProfile}
+          profile={profile}
+          key={profile.id}
+          user={user}/> : null}
+      </div>
     </div>
   )
 }
---
+++ client/src/forms/EditProfileForm.js
@@ -0,0 +1,9 @@
+function EditProfileForm(){
+    return(
+        <div>
+            <h1> Edit Profile Form</h1>
+        </div>
+    )
+}
+
+export default EditProfileForm
