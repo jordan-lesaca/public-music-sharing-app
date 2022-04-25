45d9c19271c6904a8d055301c2f098b997145c0d
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-04-25 14:28:15 -0700

    removed Link hook from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { useEffect, useState } from 'react'
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import Login from './components/Login'
 import Songs from './components/Songs'
