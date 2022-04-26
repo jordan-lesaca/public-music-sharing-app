013f5131c015e908aa5d79f0e2bdbaa2d98cb937
Jordan Lesaca <Jordanlesaca@gmail.com>
2022-04-25 17:00:28 -0700

    added code to profile and edit profile

--- client/src/forms/EditProfileForm.js
+++ client/src/forms/EditProfileForm.js
@@ -1,9 +1,66 @@
-function EditProfileForm(){
-    return(
-        <div>
-            <h1> Edit Profile Form</h1>
-        </div>
+import { useState } from 'react'
+
+function EditProfileForm({ user, profile, handleEditButton, editProfile }){
+  const [ name, setName ] = useState(profile.name)
+  const [ favorite_genre, setFavorite_Genre ] = useState(profile.favorite_genre)
+  const [ pronouns, setPronouns ] = useState(profile.pronouns)
+
+  function handleSubmit(){
+    setName("")
+    setFavorite_Genre("")
+    setPronouns("")
+    
+    fetch(`/profiles/${user.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+        body: JSON.stringify({
+          name, favorite_genre,
+          pronouns,
+          user_id: user.id
+        }),
+      })
+        .then(r => r.json())
+        .then(p =>  { 
+        handleEditButton() 
+        editProfile(p)
+    })
+  }
+
+  return(
+    <div className="form-container">
+      <h1 className="form-header">Edit Form:</h1>
+      <form className="form" onSubmit={e => handleSubmit(e)} >
+        <label>Name: </label>
+          <input 
+          type="text" 
+          id="name" 
+          value={name} 
+          onChange={e =>
+          setName(e.target.value)}/> 
+          <br/>
+        <label className="login-label">Favorite Genre: </label>
+          <input 
+          type="text" 
+          id="favorite_genre" 
+          value={favorite_genre} 
+          onChange={e =>
+          setFavorite_Genre(e.target.value)}/> 
+          <br/>
+        <label>Pronouns: </label>
+          <input 
+          type="text" 
+          id="pronouns" 
+          value={pronouns} 
+          onChange={e =>
+          setPronouns(e.target.value)}/> 
+          <br/>
+        <button className="button" type="submit" > Submit </button>
+        <br/><br/> 
+      </form>
+    </div>
     )
-}
+  }
 
 export default EditProfileForm
--- client/src/forms/ProfileForm.js
+++ client/src/forms/ProfileForm.js
@@ -1,9 +1,71 @@
-function ProfileForm(){
-    return (
-        <div> 
-            <h1> Profile Form</h1>
-        </div>
-    )
+import React, { useState } from "react"
+
+function ProfileForm({user, addProfile}){
+  const [ name, setName ] = useState("")
+  const [ favorite_genre, setFavorite_Genre ] = useState("")
+  const [ pronouns, setPronouns ] = useState("")
+  const [errors, setErrors] = useState([])
+
+  function handleSubmit() {
+    setName("")
+    setFavorite_Genre("")
+    setPronouns("")
+
+    fetch("/profiles", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        favorite_genre,
+        pronouns,
+        user_id: user.id
+      }),
+    }).then(r => {
+      if (r.ok) {
+        r.json().then(profile => 
+          addProfile(profile))
+      } else {
+        r.json().then(err => 
+          setErrors(err.errors))
+      }
+    })
+  }
+
+  return(
+    <div className="form-container">
+      <h1 className="h1-container">Profile Form:</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <label > name: </label>
+            <input 
+            type="text" 
+            id="name" 
+            value={name} 
+            onChange={e =>
+            setName(e.target.value)}/> 
+            <br/>
+          <label className="login-label"> favorite genre: </label>
+            <input 
+            type="text" 
+            id="favorite_genre" 
+            value={favorite_genre} 
+            onChange={e =>
+            setFavorite_Genre(e.target.value)}/> 
+            <br/>
+          <label > Pronouns: </label>
+            <input 
+            type="text" 
+            id="pronouns" 
+            value={pronouns} 
+            onChange={e =>
+            setPronouns(e.target.value)}/> 
+            <br/>
+          <button className="button" type="submit"> Submit </button>
+            <br/><br/> <p className="errors">{errors} {console.log(errors)}</p>
+        </form>
+    </div>
+  )
 }
 
 export default ProfileForm
